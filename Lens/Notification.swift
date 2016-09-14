97590c49b4a1c1d022b664e986c2b11094772b02
Mac <MacMini@Mac-mini-Mac.local>
2016-09-14 18:05:52 +0300

    create notifications

---
+++ Lens/Notification.swift
@@ -0,0 +1,13 @@
+//
+//  Notification.swift
+//  Lens
+//
+//  Created by Mac on 9/13/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+import UIKit
+
+class Notification: UILocalNotification {
+
+}
