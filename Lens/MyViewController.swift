452741389adfc93fae6aeb086ce27eda70e2ab47
Mac <MacMini@Mac-mini-Mac.local>
2016-09-20 10:40:06 +0300

    modified xib view

--- Lens/MyViewController.swift
+++ Lens/MyViewController.swift
@@ -9,10 +9,9 @@
 import UIKit
 import RealmSwift
 
-class MyViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+class MyViewController: UIViewController {
     var arrayOfPasks: Results<Pask>!
     var arrayOfDates: [NSDate] = []
-    var arrayForCollection: [Collection] = []
     
     @IBOutlet weak var collectionView: View!
     
@@ -21,17 +20,18 @@ class MyViewController: UIViewController, UICollectionViewDelegateFlowLayout, UI
         let notificationSettings = UIUserNotificationSettings(forTypes: [.Alert , .Badge, .Sound], categories: nil)
         UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
         getArrayOfDates { object in
-            self.arrayForCollection = HelperPask.numberOfLenses(self.arrayOfPasks)
-            self.collectionView.collectionView.registerNib(UINib(nibName: "CollectionViewCell",bundle: nil), forCellWithReuseIdentifier: "CollectionViewCell")
-            self.collectionView.collectionView.registerNib(UINib(nibName: "LastCollection",bundle: nil), forCellWithReuseIdentifier: "LastCollection")
-            let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-                    layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
-                    layout.itemSize = CGSize(width: 70, height: 150)
-            self.collectionView.collectionView.setCollectionViewLayout(layout, animated: false)
-            self.collectionView.collectionView.delegate = self
-            self.collectionView.collectionView.dataSource = self
+          
+            self.collectionView.arrayOfPasks = HelperPask.numberOfLenses(self.arrayOfPasks)
+            self.collectionView.onTouch = { () in
+            self.performSegueWithIdentifier("tableView", sender: self)
+            }
             self.collectionView.collectionView.reloadData()
         }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
     func getArray(obj: (Results<Pask>) -> ()){
@@ -40,6 +40,7 @@ class MyViewController: UIViewController, UICollectionViewDelegateFlowLayout, UI
             obj(self.arrayOfPasks)
         })
     }
+    
     func getArrayOfDates(obj: ([NSDate]) -> ()){
         getArray {object in
             if !object.isEmpty {
@@ -59,48 +60,5 @@ class MyViewController: UIViewController, UICollectionViewDelegateFlowLayout, UI
             obj(self.arrayOfPasks)
         })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-  
-        return arrayOfPasks.count + 1
-    }
-    
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        if indexPath.item == arrayForCollection.count  {
-             let cell = collectionView.dequeueReusableCellWithReuseIdentifier("LastCollection", forIndexPath: indexPath) as! LastCollection
-            
-        return cell
-        }
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath: indexPath) as! CollectionViewCell
-        cell.nameLabel.text = arrayForCollection[indexPath.item].name
-        cell.numberLabel.text = String(arrayForCollection[indexPath.item].number)
-        return cell
-    }
-   
-    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        if indexPath.item == arrayForCollection.count  {
-    
-         performSegueWithIdentifier("tableView", sender: self)
-        }
-}
 }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-
-  */
--- Lens/View.swift
+++ Lens/View.swift
@@ -8,29 +8,70 @@
 
 import UIKit
 
-class View: UIView {
+class View: UIView, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
     var view: UIView!
     var arrayOfPasks: [Collection] = []
-    typealias CollectionAction = (Bool) -> ()
+    typealias CollectionAction = () -> ()
     @IBOutlet weak var collectionView: UICollectionView!
     var nibName: String = "View"
     var onTouch: CollectionAction?
+    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
         setup()
+ 
     }
+    
     func setup() {
         view = loadViewFromNib()
         view.frame = bounds
         view.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
         addSubview(view)
+        self.collectionView.registerNib(UINib(nibName: "CollectionViewCell",bundle: nil), forCellWithReuseIdentifier: "CollectionViewCell")
+        self.collectionView.registerNib(UINib(nibName: "LastCollection",bundle: nil), forCellWithReuseIdentifier: "LastCollection")
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 70, height: 150)
+        self.collectionView.setCollectionViewLayout(layout, animated: false)
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.reloadData()
     }
     
     func loadViewFromNib() -> UIView {
+       
         let bundle = NSBundle(forClass: self.dynamicType)
         let nib = UINib(nibName: nibName, bundle: bundle)
         let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        
         return view
     }
-
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return arrayOfPasks.count + 1
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        if indexPath.item == arrayOfPasks.count  {
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier("LastCollection", forIndexPath: indexPath) as! LastCollection
+            
+            return cell
+        }
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath: indexPath) as! CollectionViewCell
+        cell.nameLabel.text = arrayOfPasks[indexPath.item].name
+        cell.numberLabel.text = String(arrayOfPasks[indexPath.item].number)
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.item == arrayOfPasks.count {
+          self.onTouch!()
+        }
+    }
 }
