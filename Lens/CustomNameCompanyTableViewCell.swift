08570d015fbe1435437695754c47077697d02672
Asmins <admin@MacBook-Air-Asmins.local>
2016-09-20 12:04:20 +0300

    Create customCell

---
+++ Lens/CustomNameCompanyTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomNameCompanyTableViewCell.swift
+//  Lens
+//
+//  Created by admin on 20.09.16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+import UIKit
+
+class CustomNameCompanyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Lens/NameCompanyViewController.swift
+++ Lens/NameCompanyViewController.swift
@@ -18,7 +18,6 @@ class NameCompanyViewController: UIViewController {
         self.title = "Name Company"
         self.setupTableView(tableView)
         self.viewModel.parseLocalJSON()
-        print(self.viewModel.arrayCompanies)
         // Do any additional setup after loading the view.
     }
 
@@ -28,6 +27,7 @@ extension NameCompanyViewController {
     
     func setupTableView(tableView:UITableView) {
         tableView.registerNib(UINib(nibName: "NameCompanyTableViewCell",bundle: nil), forCellReuseIdentifier: "NameCompany")
+        tableView.registerNib(UINib(nibName: "CustomNameCompanyTableViewCell",bundle: nil), forCellReuseIdentifier: "CustomCell")
     }
     
 }
@@ -35,30 +35,77 @@ extension NameCompanyViewController {
 extension NameCompanyViewController: UITableViewDataSource {
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.viewModel.arrayCompanies.count
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 1
+        case 2:
+            return self.viewModel.arrayCompanies.count
+        default:
+            return 0
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("NameCompany") as! NameCompanyTableViewCell
-        cell.nameCompanyLabel.text = self.viewModel.arrayCompanies[indexPath.row].name
+        
+        let cell: UITableViewCell!
+        
+        switch indexPath.section {
+        case 0:
+            let aCell = tableView.dequeueReusableCellWithIdentifier("CustomCell") as! CustomNameCompanyTableViewCell
+            aCell.nameLabel.text = "Tapp on me"
+            cell = aCell
+        case 1:
+            let aCell = tableView.dequeueReusableCellWithIdentifier("NameCompany") as! NameCompanyTableViewCell
+            aCell.nameCompanyLabel.text = "Data From Data Base" 
+            cell = aCell
+        case 2:
+            let aCell = tableView.dequeueReusableCellWithIdentifier("NameCompany") as! NameCompanyTableViewCell
+            aCell.nameCompanyLabel.text = self.viewModel.arrayCompanies[indexPath.row].name
+            cell = aCell
+        default:
+            cell = nil
+            print("Some Error")
+        }
         return cell
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 3
     }
-    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Please enter company manual"
+        case 1:
+            return "Choose from DB"
+        case 2:
+            return "Choose from popular"
+        default:
+            print("Error")
+        }
+        return nil
+    }
 }
 
 
 extension NameCompanyViewController: UITableViewDelegate {
 
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        switch indexPath.section {
+        return 45
+    }
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        switch section {
         case 0:
-            return 45
+            return 30
+        case 1:
+            return 15
+        case 2:
+            return 15
         default:
-            return 0
+            print("Error")
         }
+        return 0
     }
 }
